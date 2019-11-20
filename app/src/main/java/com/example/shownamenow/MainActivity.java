1ac676bf4d55c5044adda06811b2ff5ca4ffcb47
Nicole Balestrieri <becominglocal@gmail.com>
2019-11-19 22:50:03 -0600

    Completed simple name recall application

---
+++ app/src/main/java/com/example/shownamenow/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.shownamenow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button myButton;
+    private TextView showText;
+    private EditText enterName;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myButton = findViewById(R.id.button);
+        enterName = findViewById(R.id.editText);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+
+                String name = enterName.getText().toString();
+                showText.setText("Hello " + name);
+            }
+        });
+        showText = findViewById(R.id.textView);
+
+
+    }
+}
